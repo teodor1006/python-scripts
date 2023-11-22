458caaa30ea67bf83fd351a1ded76a0edcde0560
teodor1006 <tedi_montana@abv.bg>
2023-11-22 09:19:34 +0200

    Internet-Speed-Tester

---
+++ InternetSpeedTester/speedchecker.py
@@ -0,0 +1,19 @@
+# pip install speedtest-cli
+import speedtest as st
+
+server = st.Speedtest()
+server.get_best_server()
+
+# Test Download Speed
+down = server.download()
+down = down / 1000000
+print(f"Download Speed: {down} Mb/s")
+
+# Test Upload Speed
+up = server.upload()
+up = up / 100000
+print(f"Upload Speed: {up} Mb/s")
+
+# Test Ping
+ping = server.results.ping
+print(f"Ping Speed: {ping}")
